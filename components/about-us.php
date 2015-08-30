bdafa1ee532e850624f30c7f9303e7205f4245bc
Thomas Freeborough <4newmusic@googlemail.com>
2015-08-30 20:39:42 +0100

    About us and background
    Added information to the about us page, and a free background for people
    to use

--- components/about-us.php
+++ components/about-us.php
@@ -4,3 +4,16 @@
  */
 ?>
 <h1>About us!</h1>
+<p>We're a team of lol players who have been playing league of legends for years, with a combined amount of wins over 3,000. We've seen the game we love go through many different phases.
+With the release of the new item set documentation, we felt it was our time to shine to make a tool that all players can use and enjoy.</p>
+<p>Who we are:</p>
+<ul>
+    <li>Angrytoad (EUW)</li>
+    <li>Jonny le rapiste (EUW)</li>
+</ul>
+<p>We hope you enjoy using the site and we plan on adding a bunch of extra functionality if the site starts getting used. We hope to one day be used in the same way that people
+    use lolnexus or lolking.</p>
+<h2>Site Development</h2>
+<p>You can find a public gitHub repo of the whole site <a href="https://github.com/angrytoad/ItemFeed" target="_blank">here</a></p>
+<h2>Freebies</h2>
+<p>I used photoshop to create a nice little background you can use for your desktop, you can find it <a href="/img/free/Background.png" target="_blank">here</a></p>
